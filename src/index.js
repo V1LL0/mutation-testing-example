bb5f30122ba292dce1b040ceb7db71643e8dd0d8
Valerio Cestarelli <valerio.cestarelli@wunderflats.com>
2023-07-21 12:13:44 +0200

    max number with equivalent mutant example

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@
 export const isNonNegative = (a) => {
     return a >= 0;
 }
+
+export const max = (a, b) => {
+    return a < b ? b : a;
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,6 @@
 import {
     isNonNegative,
+    max
 } from "../src/index";
 
 
@@ -17,5 +18,17 @@ describe("Comparisons", () => {
     it("-3 should be negative", () => {
         const result = isNonNegative(-3);
         expect(result).toBe(false);
+    });
+});
+
+describe('Max', () => {
+    it('should return 4 for max(4, 3)', () => {
+        expect(max(4, 3)).toBe(4);
+    });
+    it('should return 4 for max(3, 4)', () => {
+        expect(max(3, 4)).toBe(4);
+    });
+    it('should return 3 for max(3, 3)', () => {
+        expect(max(3, 3)).toBe(3);
     });
 });
